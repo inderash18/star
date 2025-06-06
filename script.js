8f9982b5f16d4b2eb8ff688f391681676c78d954
inderash18 <minderash@gmail.com>
2025-06-06 15:49:06 +0530

    hii

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('hii');
+console.log(5=='5')
+console.log(5==='5');
+let i=0
+do{
+    i++;
+    console.log(i);
+}while(i<=4)
+    function jack(age,brain)
+    {
+        return `hello${age} years${brain} stupid`
+    }
+    console.log(jack(40,'old'));
+
+    function showname()
+    {
+        const name=document.getElementById('name').value document.getElementById('result').innertext='hello ${name}'
+    } 
